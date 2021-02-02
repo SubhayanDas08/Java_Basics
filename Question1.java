b317e1997addc561317192784a9fd695350400cb
SubhayanDas08 <nips.subhayan@gmail.com>
2021-02-02 16:47:12 +0530

    2nd Feb  2021

---
+++ Question1.java
@@ -0,0 +1,8 @@
+public class Question1 {
+    public static void main(String[] args) {
+        System.out.println("Name:   Subhayan Das");
+        System.out.println("Roll:   1905214");
+        System.out.println("Class:  CS4");
+        System.out.println("Branch: CSE");
+    }
+}
