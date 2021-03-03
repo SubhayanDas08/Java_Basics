637556840b2252150859c7b481b55486522fc6c9
SubhayanDas08 <nips.subhayan@gmail.com>
2021-03-03 18:52:26 +0530

    3rd Mar  2021

---
+++ Lab7/total_earnings.java
@@ -0,0 +1,9 @@
+import marketing.sales;
+
+public class total_earnings {
+    public static void main(String[] args) {
+        sales s = new sales(101, " Atharv ");
+        double total = s.earnings(10100) + s.tallowance(1000);
+        System.out.println(" Emp ID : " + s.getEmpId() + " Total Earnings : " + total);
+    }
+}
