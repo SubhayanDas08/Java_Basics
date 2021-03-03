4534600105b6e8f602665293959aadc76b59b943
SubhayanDas08 <nips.subhayan@gmail.com>
2021-03-03 18:48:47 +0530

    3rd Mar  2021

---
+++ Lab7/general/employee.java
@@ -0,0 +1,15 @@
+package Lab7.general;
+
+public class employee {
+    protected int empid;
+    private String ename;
+
+    public employee(int empid, String ename) {
+        this.empid = empid;
+        this.ename = ename;
+    }
+
+    public double earnings(int basic) {
+        return basic + 0.8 * basic + 0.15 * basic;
+    }
+}
